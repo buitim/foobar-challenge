8a1d61436735fca2ec3e0f1d2dab919e268d80b9
buitim <buitim@oregonstate.edu>
2020-08-16 00:53:38 -0700

    created board

--- level-2/challenge-1/solution.py
+++ level-2/challenge-1/solution.py
@@ -1,2 +1,41 @@
+BOARD_SIZE = 8                                                                              # The number of nodes in each row on the boards
+ALLOWED_MOVES = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]    # List of tuples describing the allowed movements
+
+class Node:
+    def __init__(self, x, y):
+        # Define coordinates
+        self.x = x
+        self.y = y
+        self.isVisited = False
+
+class Board:
+    def __init__(self, size, allowedMoves):
+        self.size = size
+        self.allowedMoves = allowedMoves
+        
+        # Create the board
+        # Source: https://stackoverflow.com/a/2397150
+        self.board = [[-1 for col in range(self.size)] for row in range(self.size)]
+        
+        # Initialize values on board
+        value = 0
+        for row in range(self.size):
+            for col in range(self.size):
+                self.board[row][col] = value
+                value += 1
+        
+        for row in self.board:
+            print(row)
+
 def solution(src, dest):
-    #Your code here
+    # NOTE: This is a BFS problem (shortest path)
+    # TODO: Create the board
+    board = Board(BOARD_SIZE, ALLOWED_MOVES)
+
+    # TODO: Set start point
+
+    # TODO: Set destination point
+
+    # TODO: Get the shortest path
+
+solution(0, 30)
